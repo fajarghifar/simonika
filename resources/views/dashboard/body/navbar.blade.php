258a84c794df2032b0e8f944c13187db0fb37a85
Fajar Ghifari <fajarghifari3@gmail.com>
2023-11-30 13:04:26 +0700

    doc : install laravolt/avatar

--- resources/views/dashboard/body/navbar.blade.php
+++ resources/views/dashboard/body/navbar.blade.php
@@ -11,11 +11,12 @@
         <div class="navbar-nav flex-row order-md-last">
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-                    <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                    <span class="avatar avatar-sm" style="background-image: url({{ Avatar::create(Auth::user()->name)->toBase64() }})"></span>
                     <div class="d-none d-xl-block ps-2">
-                        <div>Paweł Kuna</div>
-                        <div class="mt-1 small text-muted">UI Designer</div>
+                        <div>{{ Auth::user()->name }}</div>
+                        <div class="mt-1 small text-muted">{{ Auth::user()->email }}</div>
                     </div>
+
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                     <a href="./profile.html" class="dropdown-item">Profile</a>
